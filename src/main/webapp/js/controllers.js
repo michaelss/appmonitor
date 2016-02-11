fb99fcf06b7751d93576fad5160ee9e0c567a218
Michael Schuenck <michaelss@gmail.com>
2016-02-10 22:32:42 -0300

    Loading servers and apps dinamically from the back-end.

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -2,13 +2,21 @@ var controllers = angular.module('appmonitorControllers', []);
 
 controllers.controller('ServersCtrl', [ '$scope', '$http',
 		function($scope, $http) {
-			var serverList = this;
-			
-			$http.get('services/servers/1/apps').success(function(data) {
-				serverList.apps = data;
+			$scope.servers = [];
+			$http.get('services/servers').success(function(data) {
+				$scope.servers = data;
 			});
-//			serverList.apps = [{"name":"appmonitor.war","active":false},
-//			                {"name":"padlog.war","active":true}]; 
+			
+			$scope.fn = function(param){
+				alert('Fui clicado...' + param);
+			};
+			$scope.loadApps = function(server){
+				$http.get('services/servers/' + server.id + '/apps').success(function (response) { //then(function successCallback(response) {
+					server.apps = response;
+					server.visible = !server.visible;
+//					server.viewHideAppsLabel = (server.visible) ? "Hide apps" : "View apps";
+				});
+			};
 		} ]);
 
 controllers.controller('ServerDetailCtrl', [ '$scope', '$routeParams',
